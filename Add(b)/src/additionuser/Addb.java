a73ecb7733aebd006bf6188b2b2d613eb047e9cb
Acer <Acer@DESKTOP-F9ONNJE>
2022-02-13 19:57:31 +0530

    Add(b)

---
+++ Add(b)/src/additionuser/Addb.java
@@ -0,0 +1,21 @@
+package additionuser;
+
+import java.util.Scanner;
+
+public class Addb {
+
+	public static void main(String[] args) {
+		int num1,num2,num3;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ENTER THE FIRST NUMBER");
+		num1= sc.nextInt();
+		System.out.println("enter the second");
+		num2= sc.nextInt();
+	    num3=num1+num2;
+		System.out.println("the sum is " +num3);
+	}
+		
+		
+	}
+
+
